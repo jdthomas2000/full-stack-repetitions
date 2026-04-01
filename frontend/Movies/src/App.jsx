79e89477350dd7a0b880e5e0447a13d2e37dd17e
Jacob Thomas <jdthomas2000@gmail.com>
2026-04-01 14:41:01 -0700

    added post functionality

--- frontend/Movies/src/App.jsx
+++ frontend/Movies/src/App.jsx
@@ -4,12 +4,37 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [post, setPost] = useState({
+    title: "",
+    main_character: "",
+    year_released: "",
+  });
 
   useEffect(() => {
     fetch("http://localhost:8080/movies")
       .then((res) => res.json())
       .then((data) => setMovies(data));
   }, []);
+
+  const fetchOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(post),
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setPost({ ...post, [name]: value });
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const res = await fetch("http://localhost:8080/movies/", fetchOptions);
+      const data = await res.json();
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   if (movies.length === 0) return <h1>loading...</h1>;
 
@@ -24,6 +49,29 @@ function App() {
           </div>
         ))}
       </div>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          name="title"
+          value={post.title}
+          onChange={handleChange}
+          placeholder="title"
+        ></input>
+        <input
+          name="main_character"
+          value={post.main_character}
+          onChange={handleChange}
+          placeholder="main_character"
+        ></input>
+        <input
+          name="year_released"
+          value={post.year_released}
+          onChange={handleChange}
+          placeholder="year_released"
+        ></input>
+
+        <button type="submit">Send Data</button>
+      </form>
     </>
   );
 }
